463f559b97528f6b495f4f40d0f94b9694fb4ce0
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-02-16 19:48:16 +0100

    receibing and painting numbers

--- calculator-logic/index.js
+++ calculator-logic/index.js
@@ -1,13 +1,8 @@
 require("dotenv").config();
 const debugg = require("debug")("calculator-app:root");
 const http = require("http");
-const askUserNumbers = require("./askUserNumbers");
+const operateNumbers = require("./operateNumbers");
 const parseToNumber = require("./parseToNumbers");
-
-const [userNumberOne, userNumberTwo] = require("./getInputByModifiers");
-
-const nums = parseToNumber(userNumberOne, userNumberTwo);
-askUserNumbers(...nums);
 
 const port = process.env.MY_PORT;
 
@@ -19,5 +14,14 @@ myServer.listen(port, () => {
 
 myServer.on("request", (request, response) => {
   debugg(`Resquest arrived at ${request.url} with method ${request.method}`);
-  response.write("<h1>the magician of programming<h1>");
+  const queryNumber1 = request.url.match(/([^?]*)\?a=(\d*)/)[2];
+  const queryNumber2 = request.url.match(/([^?]*)\&b=(\d*)/)[2];
+  const numbers = parseToNumber(queryNumber1, queryNumber2);
+  const [sum, rest, multiplication, division] = operateNumbers(...numbers);
+
+  response.write(`<h1>My amazing calculator<h1>
+  <h2>The sum is ${sum}<h2>
+  <h2>The rest is ${rest}<h2>
+  <h2>The multiplication is ${multiplication}<h2>
+  <h2>The division is ${division}<h2>`);
 });
